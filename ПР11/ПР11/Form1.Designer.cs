c888ce3e03a7e4089383a14d291d297b87ecc81d
eaz1lyyy <124042552+eaz1lyyy@users.noreply.github.com>
2023-05-23 15:58:50 +0300

    Add files via upload

---
+++ ПР11/ПР11/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace ПР11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picFrame = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picFrame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picFrame
+            // 
+            this.picFrame.Location = new System.Drawing.Point(32, 77);
+            this.picFrame.Name = "picFrame";
+            this.picFrame.Size = new System.Drawing.Size(415, 361);
+            this.picFrame.TabIndex = 0;
+            this.picFrame.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 15;
+            this.trackBar1.Location = new System.Drawing.Point(141, 20);
+            this.trackBar1.Maximum = 50;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.trackBar1.Size = new System.Drawing.Size(306, 45);
+            this.trackBar1.SmallChange = 15;
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.TickFrequency = 3;
+            this.trackBar1.Value = 5;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 450);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picFrame);
+            this.Name = "Form1";
+            this.Text = "Анимация";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picFrame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picFrame;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ПР11/ПР11/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ПР11
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap[] Frames;
+        private int FrameNum = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            trackBar1.Value = timer1.Interval;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Frames = new Bitmap[18];
+            for (int i = 0; i < 18; i++)
+            {
+                Frames[i] = new Bitmap("Frame" + i + ".png");
+            }
+            picFrame.Image = Frames[FrameNum];
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            FrameNum = ++FrameNum % Frames.Length;
+            picFrame.Image = Frames[FrameNum];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            if (timer1.Enabled) button1.Text = "Stop";
+            else button1.Text = "Start";
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = trackBar1.Value;
+        }
+    }      
+}   
